a3984f537c9cc0688c2359b4be19de1472b01614
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-01-08 08:49:59 -0400

    feat: add greet() to return personalized greeting based on owner status

---
+++ cpp/8kyu/grasshopper-personalized-message.cpp
@@ -0,0 +1,18 @@
+/*
+
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+*/
+
+#include <string>
+using namespace std;
+
+string greet(const string& name, const string& owner) {
+return "Hello " + string(name == owner ? "boss" : "guest");
+}
