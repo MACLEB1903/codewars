ebc34f0a25e67526c05cc6ff1239375f2b83d6ae
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-01-16 15:30:51 -0400

    feat: add number() to prepend line numbers to a list of strings

---
+++ cpp/8kyu/testing-1-2-3.cpp
@@ -0,0 +1,27 @@
+/*
+
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+
+*/
+
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> number(const vector<string>& lines) {
+    vector<string> numbered;
+    for (size_t i = 0; i < lines.size(); ++i) {
+        numbered.push_back(to_string(i + 1) + ": " + lines[i]);
+    }
+    return numbered;
+}
