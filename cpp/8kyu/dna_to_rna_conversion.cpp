ff8cb3bd63ee1dd791e5446a8720aae304f96ef1
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-01-28 21:44:28 -0400

    feat: add DNAtoRNA() solution for kata 5556282156230d0e5e000089

---
+++ cpp/8kyu/dna_to_rna_conversion.cpp
@@ -0,0 +1,34 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/5556282156230d0e5e000089/train/cpp
+
+
+Instructions:
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+Create a function which translates a given DNA string into RNA.
+
+For example:
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+*/
+
+#include <string>
+using namespace std;
+
+#include <string>
+using namespace std;
+
+string DNAtoRNA(string dna) {
+    for (char &c : dna) {   // note the & here
+        if (c == 'T') {
+            c = 'U';
+        }
+    }
+    return dna;
+}
