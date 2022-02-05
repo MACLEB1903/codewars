5b9306b4be1b0932c6ced2158ab9a8e47897d290
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-02-04 20:19:41 -0400

    feat: add Example() solution for kata 5ab6538b379d20ad880000ab

---
+++ cpp/8kyu/area_or_perimeter.cpp
@@ -0,0 +1,25 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/5ab6538b379d20ad880000ab/train/cpp
+
+
+Instructions:
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+Example(Input1, Input2 --> Output):
+6, 10 --> 32
+3, 3 --> 9
+
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+*/
+
+int area_or_perimeter(int l , int w) {
+  if (l == w) return l * w;
+  return (l + w) * 2;
+}
