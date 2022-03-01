7e47deec1df5c42658fd28bd8e43a7852f950935
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-02-28 20:20:48 -0400

    feat: add opposite() solution for kata 56dec885c54a926dcd001095

---
+++ cpp/8kyu/opposite_number.cpp
@@ -0,0 +1,22 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/56dec885c54a926dcd001095/train/cpp
+
+
+Instructions: 
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+1: -1
+14: -14
+-34: 34
+
+*/
+
+int opposite(int number) {
+  return -number;
+}
