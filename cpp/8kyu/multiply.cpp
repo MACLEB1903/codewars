8b386141ac538b2affabee88c60d2928d80083cd
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-03-01 12:02:41 -0400

    feat: add multiply() solution for kata 50654ddff44f800200000004

---
+++ cpp/8kyu/multiply.cpp
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/50654ddff44f800200000004/train/cpp
+
+
+Instructions:
+The code does not execute properly. Try to figure out why.
+
+*/
+
+int multiply(int a, int b) {
+  return a * b;
+}
