7d737b7f9694508451225ffe61bb1c77f2b363ff
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-03-03 20:54:40 -0400

    feat: add get-character-from-ascii-value.cpp solution for kata 55ad04714f0b468e8200001c

---
+++ cpp/8kyu/get-character-from-ascii-value.cpp
@@ -0,0 +1,24 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/cpp
+
+
+Instructions:
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+
+For ASCII table, you can refer to http://www.asciitable.com/
+
+*/
+
+char get_char(int i) {
+    return int(i);
+  }
