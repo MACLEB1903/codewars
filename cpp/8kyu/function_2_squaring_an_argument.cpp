db45b7e54b9e4355c3cc9f092b08fd0908d57249
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-03-13 20:42:09 -0400

    feat: add square() solution for kata 523b623152af8a30c6000027

---
+++ cpp/8kyu/function_2_squaring_an_argument.cpp
@@ -0,0 +1,14 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/523b623152af8a30c6000027/train/cpp
+
+Instructions:
+This function takes a number as an argument and returns the square of it.
+
+*/
+
+int square(int param) { return param * param;};
