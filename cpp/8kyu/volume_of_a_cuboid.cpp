25d99a9037d4f8c0fc54dbb4be073e23549209a7
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-04-04 20:25:15 -0400

    feat: add getVolumeOfCuboid() solution for kata 58261acb22be6e2ed800003a

---
+++ cpp/8kyu/volume_of_a_cuboid.cpp
@@ -0,0 +1,18 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/cpp
+
+
+Instructions:
+Bob needs a fast way to calculate the volume of a rectangular cuboid with three values: the length, width and height of the cuboid.
+Write a function to help Bob with this calculation.
+
+*/
+
+double getVolumeOfCuboid(double l, double w, double h) {
+    return l*w*h;
+}
