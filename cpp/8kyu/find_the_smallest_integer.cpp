3e7eaaa2f7ab0d1ee2732635b78beb83183e57e2
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-05-10 20:34:00 -0400

    feat: add find_the_smallest_integer.cpp solution for kata 57f781872e3d8ca2a000007e

---
+++ cpp/8kyu/find_the_smallest_integer.cpp
@@ -0,0 +1,33 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/cpp
+
+
+Instructions:    
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+    
+*/
+
+
+#include <vector>
+using namespace std; 
+
+int findSmallest(vector <int> list){
+  int min = list[0];
+  
+  for (auto it : list) {
+    if (it < min)
+      min = it;
+  }
+  
+  return min;
+}
