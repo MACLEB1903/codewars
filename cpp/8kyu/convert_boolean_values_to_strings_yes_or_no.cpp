0e1aca46d7bf593da5eec6f51946c717bfaaacfe
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-05-13 09:22:47 -0400

    feat: add bool_to_word() solution for kata 53369039d7ab3ac506000467

---
+++ cpp/8kyu/convert_boolean_values_to_strings_yes_or_no.cpp
@@ -0,0 +1,18 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/53369039d7ab3ac506000467/train/cpp
+
+
+Instructions:
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+*/
+
+#include <string>
+using namespace std;
+
+string bool_to_word(bool value){return value ? "Yes" : "No";}
