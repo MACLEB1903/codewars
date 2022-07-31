06fa80d203b77913186fdcefe3a1d94ebffdcdb8
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-07-30 20:15:00 -0400

    feat: add count_of_positives_sum_of_negatives.cpp solution for kata 576bb71bbbcf0951d5000044

---
+++ cpp/8kyu/count_of_positives_sum_of_negatives.cpp
@@ -0,0 +1,39 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/cpp
+
+
+Instructions:
+Given an array of integers.
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+If the input is an empty array or is null, return an empty array.
+
+Example:
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+*/
+
+#include <vector>
+using namespace std;
+
+vector<int> countPositivesSumNegatives(const vector<int>& input)
+{
+    if (input.empty()) return {};
+
+    int countPositives = 0;
+    int sumNegatives = 0;
+
+    for (int num : input) {
+        if (num > 0) {
+            countPositives++;
+        } else if (num < 0) {
+            sumNegatives += num;
+        }
+    }
+
+    return {countPositives, sumNegatives};
+}
