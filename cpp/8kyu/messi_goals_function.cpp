2b3380be3e113f3879a5031d2afce88a2d5bf480
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-08-02 20:48:16 -0400

    feat: add goals() solution for kata 55f73be6e12baaa5900000d4

---
+++ cpp/8kyu/messi_goals_function.cpp
@@ -0,0 +1,28 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/cpp
+
+
+Instructions: 
+Messi goals function
+
+Messi is a soccer player with goals in three leagues:
+LaLiga
+Copa del Rey
+Champions
+
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+For example:
+5, 10, 2  -->  17
+
+*/
+
+int goals (int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
