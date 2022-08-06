6e03bbceb007a0c03e6982f1608fa948cf6ccaf3
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-08-05 21:50:29 -0400

    feat: add plane() solution for kata 643af0fa9fa6c406b47c5399

---
+++ cpp/8kyu/quadrants.cpp
@@ -0,0 +1,33 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/643af0fa9fa6c406b47c5399/train/cpp    
+
+Instructions: 
+Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
+
+Examples:
+ (1, 2)     => 1
+ (3, 5)     => 1
+ (-10, 100) => 2
+ (-1, -9)   => 3
+ (19, -56)  => 4
+  
+*/
+
+
+int quadrant(int x, int y) {
+  if (x > 0 && y > 0)
+    return 1;
+  if (x < 0 && y > 0)
+    return 2;
+  if (x < 0 && y < 0)
+    return 3;
+  if (x > 0 && y < 0)
+    return 4;
+  
+  return 0;
+}
