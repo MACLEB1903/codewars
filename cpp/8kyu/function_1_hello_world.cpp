351498f61838b289046d98c91fb88f843035fc8b
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-08-31 10:29:26 -0400

    feat: add greet() solution for kata 523b4ff7adca849afe000035

---
+++ cpp/8kyu/function_1_hello_world.cpp
@@ -0,0 +1,23 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/523b4ff7adca849afe000035/train/cpp
+
+
+Instructions:
+Make a simple function called greet that returns the most-famous "hello world!".
+
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+*/
+
+#include <string>
+using namespace std;
+
+string greet(){
+  return "hello world!";
+}
