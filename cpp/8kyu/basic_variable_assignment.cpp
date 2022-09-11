d9ef3cc9b33fb6ffcaf45ebea4138e32d80693f6
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-09-11 13:39:08 -0400

    feat: add basic_variable_assignment.cpp solution for kata 50ee6b0bdeab583673000025

---
+++ cpp/8kyu/basic_variable_assignment.cpp
@@ -0,0 +1,20 @@
+/* 
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/cpp
+
+
+Instructions:
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+*/
+
+#include <string>
+using namespace std;
+
+string a = "code";
+string b = "wa.rs";
+string name = a + b;
