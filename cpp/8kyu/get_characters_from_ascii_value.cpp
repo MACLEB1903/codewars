caf758380aa532c69e2314144d20f9e14a74aa28
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-10-27 19:52:23 -0400

    feat: add get_char() solution for kata 55ad04714f0b468e8200001c

---
+++ cpp/8kyu/get_characters_from_ascii_value.cpp
@@ -0,0 +1,23 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/cpp
+
+
+Instructions:  
+Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+ 
+Example:
+get_char(65)
+should return:
+'A'
+    
+*/
+
+char get_char(int i) {
+  char c = i;
+  return c;
+}
