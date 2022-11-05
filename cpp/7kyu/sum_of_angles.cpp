2cdd347302609aaeed7470f54218800fd5895e25
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-11-04 20:16:06 -0400

    feat: add sum_of_angles.cpp solution for kata 5a03b3f6a1c9040084001765

---
+++ cpp/7kyu/sum_of_angles.cpp
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/cpp
+
+
+Instructions:
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+*/
+
+int angle(int n){
+  return (n - 2) * 180;
+}
