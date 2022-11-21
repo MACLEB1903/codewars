d0d2901eb6d2f4d43b93f7191bbad1e9348b2f26
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2022-11-21 19:54:33 -0400

    feat: add double_integer() solution for kata 53ee5429ba190077850011d4

---
+++ cpp/8kyu/you_cant_code_under_pressure_1.cpp
@@ -0,0 +1,18 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source: 
+https://www.codewars.com/kata/53ee5429ba190077850011d4/train/cpp
+
+
+Instructions:
+
+Code as fast as you can! You need to double the integer and return it.
+
+*/
+
+int double_integer(int i) {
+  return i*2;
+}
