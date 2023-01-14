0e1491a543b00b8a8a4118dc135ae7c5720e8020
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-01-13 20:36:04 -0400

    feat: add WideMouthedFrog solution for kata 57ec8bd8f670e9a47a000f89

---
+++ java/8kyu/TheWideMouthedFrog.java
@@ -0,0 +1,29 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89/train/java
+
+
+Instructions:
+The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+When he meets the alligator, it then makes a tiny mouth.
+
+Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide.
+
+*/
+
+public class WideMouthedFrog{
+  public static String mouthSize(String animal){
+   if(animal.toLowerCase().equals("alligator")){
+     return "small";
+   }else{
+     return "wide";
+   }
+  }
+}
