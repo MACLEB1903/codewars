5656a0d460ee41dfbb3169c249e6487d162ff1ce
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-01-30 20:03:33 -0400

    feat: add Kata solution for kata 57241e0f440cd279b5000829

---
+++ java/8kyu/SumOfMultiples.java
@@ -0,0 +1,30 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57241e0f440cd279b5000829/train/java
+
+
+Instructions:
+Find the sum of all multiples of n below m 
+Keep in Mind
+n and m are natural numbers (positive integers)
+m is excluded from the multiples
+
+Examples:
+Kata.sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+Kata.sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+Kata.sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+Kata.sumMul(4, -7)  // throws IllegalArgumentException
+
+*/
+
+public class Kata {
+    public static long sumMul(int n, int m) {
+        if (n < 1 || m < 1) throw new IllegalArgumentException("n or m < 1");
+        int x = (m - 1) / n;
+        return (long) (n * 0.5 * x * (x + 1));
+    }
+}
