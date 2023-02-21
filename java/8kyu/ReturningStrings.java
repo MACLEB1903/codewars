920d10d28cbeb51c4b6f0f497226577b1dca395b
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-02-20 20:24:34 -0400

    feat: add Kata solution for kata 55a70521798b14d4750000a4

---
+++ java/8kyu/ReturningStrings.java
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/55a70521798b14d4750000a4/train/java
+
+
+Instructions:
+Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+*/
+
+public class Kata{
+  public static String greet(String name){
+    return "Hello, " + name + " how are you doing today?";
+  }
+}
