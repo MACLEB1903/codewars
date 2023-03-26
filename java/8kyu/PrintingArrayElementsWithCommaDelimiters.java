0215750e11a8e4c51a589a4f00d6cdfe57017dfd
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-03-26 18:38:54 -0400

    feat: add ArrayPrinter solution for kata 56e2f59fb2ed128081001328

---
+++ java/8kyu/PrintingArrayElementsWithCommaDelimiters.java
@@ -0,0 +1,27 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/java
+
+
+Instructions:
+
+Input: Array of elements
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+"h,o,l,a"
+
+Note: if this seems too simple for you try the next level
+Note2: the input data can be: boolean array, array of objects, array of string arrays, array of number arrays... 😕
+
+*/
+
+import java.util.Arrays;
+import java.util.stream.Collectors;public class ArrayPrinter {    public static String printArray(Object[] array) {
+      return Arrays.stream(array).map(Object::toString).collect(Collectors.joining(","));
+    }
+}
