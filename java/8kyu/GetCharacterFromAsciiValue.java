260dc6d92114aa23d8f3b334d2fcc51934183c05
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-04-01 20:23:01 -0400

    feat: add Ascii solution for kata 55ad04714f0b468e8200001c

---
+++ java/8kyu/GetCharacterFromAsciiValue.java
@@ -0,0 +1,26 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/java
+
+
+Instructions:
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example: 
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+
+For ASCII table, you can refer to http://www.asciitable.com/
+
+*/
+
+public class Ascii {
+  public static char getChar(int c) {
+    return (char) c;
+  }
+}
