4799322f3a821e13517dc4f94c675162c00eefb7
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-04-29 11:09:09 -0400

    feat: add School solution for kata 563e320cee5dddcf77000158

---
+++ java/8kyu/GetTheMeanOfAnArray.java
@@ -0,0 +1,27 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/563e320cee5dddcf77000158/train/java
+
+
+Instructions:
+It's the academic year's end, fateful moment of your school report.
+The averages must be calculated. All the students come to you and entreat you to calculate their average for them.
+Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+
+*/
+
+public class School{ 	public static int getAverage(int[] marks){
+		int sum = 0;
+    for(int number : marks){
+      sum+=number;
+    }
+    return Math.round(sum/marks.length);
+	}}
