571fa539ee650a3c40974d47517b5f5a2d8af6b6
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-05-24 20:08:55 -0400

    feat: add FirstClass solution for kata 5512a0b0509063e57d0003f5

---
+++ java/8kyu/APlusB.java
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5512a0b0509063e57d0003f5/train/java
+
+
+Instructions:
+Vasya Pupkin just started learning Java and C#. At first, he decided to write simple program that was supposed to sum up two small numbers (numbers and their sum fit in a byte), but it didn't work. Vasya was too sad to find out what is wrong. Help him to correct the mistake.
+
+*/
+
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+        return (byte) (a + b);
+    }
+}
