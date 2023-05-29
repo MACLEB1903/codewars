fc0b0acbf993a760a7e4e4adec72961b86dbc037
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-05-29 19:13:19 -0400

    feat: add Solution solution for kata 56200d610758762fb0000002

---
+++ java/8kyu/GrasshopperBasicFunctionFixer.java
@@ -0,0 +1,25 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56200d610758762fb0000002/train/java
+
+
+Instructions:
+Fix the function:
+
+I created this function to add five to any number that was passed in to it and return the new value.
+It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+
+*/
+
+public class Solution {
+  public static int addFive(int num) {
+    int total = num + 5;
+    return total;
+  }
+}
