f8bd66b566df802c03a9f7276acef8084674af13
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-06-08 17:27:35 -0400

    feat: add Multiply solution for kata 50654ddff44f800200000004

---
+++ java/8kyu/Multiply.java
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/50654ddff44f800200000004/train/java
+
+
+Instructions:
+This code does not execute properly. Try to figure out why.
+
+*/
+
+public class Multiply {
+    static double multiply(double a, double b) {
+      return a * b;
+    }
+}
