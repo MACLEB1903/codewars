7a6d4eb7ebce5ceef28fac86c8338ab08174cd7c
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-06-11 20:28:14 -0400

    feat: add Kata solution for kata 523b623152af8a30c6000027

---
+++ java/8kyu/Function2SquaringAnArgument.java
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/523b623152af8a30c6000027/train/java
+
+
+Instructions:
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+public class Kata{
+  public static int square(int n){
+    return n*n;
+  }
+ }
