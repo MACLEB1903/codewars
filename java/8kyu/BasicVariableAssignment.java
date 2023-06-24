c0da4f2a40008c9f9101ca24e4aa54e0595dcb3c
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-06-24 09:33:28 -0400

    feat: add Kata solution for kata 50ee6b0bdeab583673000025

---
+++ java/8kyu/BasicVariableAssignment.java
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/java
+
+
+Instructions:
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+*/
+
+public class Kata {
+    public static String a = "code";
+    public static String b = "wa.rs";
+    public static String name = a + b;
+}
