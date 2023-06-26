113bc60e7df1ff653c061eb74ef13996719c2e6d
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-06-25 20:05:54 -0400

    feat: add Kata solution for kata 56dec885c54a926dcd001095

---
+++ java/8kyu/OppositeNumber.java
@@ -0,0 +1,26 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56dec885c54a926dcd001095/train/java
+
+
+Instructions:
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+1: -1
+14: -14
+34: 34
+
+*/
+
+public class Kata
+    {
+        public static int opposite(int number)
+        {
+            return number*-1;
+        }
+    }
