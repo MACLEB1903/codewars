59120b565b81887634b4ebf64453a83cde608f4c
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-07-02 17:59:32 -0400

    feat: add capitalize_word() solution for kata 5a4295898882f3538400006f

---
+++ python/8kyu/capitalization.py
@@ -0,0 +1,30 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5a4295898882f3538400006f/train/python
+
+
+Instructions:
+Capitalization is writing a word with its first letter as a capital letter. our task in this kata is to capitalize the given word.
+
+Sample :: input >- Output :: 
+"apple" >- "Apple" .
+"demonstration" >- "Demonstration" .
+"local" >- "Local" .
+
+play by the rules : 
+1- Note, that during capitalization all the letters except the first one remains unchanged.
+2- Vector as header file and its functions are disabled . 
+
+Please , I beg your pardon , if there are any Question concerning the kata Post it as suggestion or question but not isuue , Thanks in advance .
+and if any know how to add random tests Please Leave a comment m Thanks for any Contribution .
+
+Hope You Enjoy the kata .. Keep on Learning .
+
+"""
+
+def capitalize_word(word):
+    return word.capitalize()
