6a4d42b57c2731b4ef904ffddb44d81b9d4d007b
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-07-10 21:25:16 -0400

    feat: add solution() to reverse a given string input

---
+++ python/8kyu/reversed-strings.py
@@ -0,0 +1,14 @@
+"""
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+"""
+
+def solution(string):
+  return "".join(reversed(string))
+
+def solution(str):
+  return str[::-1]
