6a0f2570a0c90b09894c6224cf4ee834dce68739
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-07-14 21:34:43 -0400

    feat: add other_angle() solution for kata 5a023c426975981341000014

---
+++ python/8kyu/third_angle_of_a_triangle.py
@@ -0,0 +1,21 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5a023c426975981341000014/train/python
+
+
+Instructions:
+You are given two interior angles (in degrees) of a triangle. 
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+Reference: https://en.wikipedia.org/wiki/Triangle
+
+"""
+
+def other_angle(a,b):
+	return 180 - a - b
