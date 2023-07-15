4fd36c185f045f2388e0a159fa0288c0c89ceaf3
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-07-15 16:31:35 -0400

    add: sp_eng() solution for kata 58dbdccee5ee8fa2f9000058

---
+++ python/8kyu/do_you_speak_english.py
@@ -0,0 +1,19 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/train/python
+
+
+Instructions:
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+Return value as boolean values, true for the string to contains "English", false for it does not.
+
+"""
+
+def sp_eng(sentence):
+	return "english" in sentence.lower()
