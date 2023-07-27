05917d05363e4131659ad8608fa1fb93ada6eebf
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-07-27 17:44:27 -0400

    feat: add find_longest() solution for kata 56f173a35b91399a05000cb7

---
+++ python/8kyu/squash_the_bugs.py
@@ -0,0 +1,17 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56f173a35b91399a05000cb7/train/python
+
+
+Instructions:
+Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+There will only be one 'longest' word.
+
+"""
+
+def find_longest(strng):
+    return max(len(a) for a in strng.split())
