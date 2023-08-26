deaa8a5dd9a99f6bf3775115e8216a975129f7d4
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-08-26 16:32:11 -0400

    add: hex_to_dec() solution for kata 57a4d500e298a7952100035d

---
+++ python/8kyu/hex_to_decimal.py
@@ -0,0 +1,16 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57a4d500e298a7952100035d/train/python
+
+
+Instructions:
+Complete the function which converts hex number (given as a string) to a decimal number.
+
+"""
+
+def hex_to_dec(s):
+	return int(s, 16)
