b3977d57eedb65ea101c405893b8bfb715ea536e
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-08-28 16:50:23 -0400

    add: basic_variable_assignment solution for kata 50ee6b0bdeab583673000025

---
+++ python/8kyu/basic_variable_assignment.py
@@ -0,0 +1,17 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/python
+
+
+Instructions:
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+"""
+
+a = "code"
+b = "wa.rs"
+name = a + b
