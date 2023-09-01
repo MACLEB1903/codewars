c5a59ca2760292975c6896e3b5f68f87c13c64fe
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-09-01 09:05:32 -0400

    feat: add maps() solution for kata 57f781872e3d8ca2a000007e

---
+++ python/8kyu/beginner_lost_without_a_map.py
@@ -0,0 +1,19 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/python
+
+
+Instructions:
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+[1, 2, 3] --> [2, 4, 6]
+
+"""
+
+def maps(a):
+	return [2 * x for x in a]	
