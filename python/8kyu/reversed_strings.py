00a6d60affe2e579dbb00518dc40f32844c49e19
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-09-17 16:21:03 -0400

    add: solution() solution for kata 5168bb5dfe9a00b126000018

---
+++ python/8kyu/reversed_strings.py
@@ -0,0 +1,19 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+
+
+Instructions:
+Complete the solution so that it reverses the string passed into it. 
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+"""
+
+def solution(string):
+    return string[::-1]
