e81a66b9f18ef7b0354c83ea7b34a3ffcb9d94eb
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-09-16 22:02:51 -0400

    feat: add divide_numbers() solution for kata 54d1c59aba326343c80000e7

---
+++ python/8kyu/incorrect_division_method.py
@@ -0,0 +1,16 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/54d1c59aba326343c80000e7/train/python
+
+
+Instructions:
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+"""
+
+def divide_numbers(x,y):
+	return x / y
