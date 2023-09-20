f81dac160cdbbbbff73cf1553f212db8852c39f9
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-09-20 16:42:55 -0400

    add: greet() solution for kata 5968644eea5c541501000110

---
+++ python/8kyu/hello_world.py
@@ -0,0 +1,21 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5968644eea5c541501000110/train/python
+
+
+Instructions:
+Given an argument that may be a String or a Number, return a simple greeting.
+
+Examples:
+hello('Noclip') => 'Hello Noclip'
+hello(13) => 'Hello 13'
+hello('there') => 'Hello there'
+
+"""
+
+def greet():
+    return "hello world!"
