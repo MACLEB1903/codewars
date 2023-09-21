9bab5c9b17fe790786547a7a956afda915de262a
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-09-21 18:34:15 -0400

    feat: add bin_to_devcimcal() solution for kata 57a5c31ce298a7e6b7000334

---
+++ python/8kyu/bin_to_decimal.py
@@ -0,0 +1,16 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/python
+
+
+Instructions:
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+"""
+
+def bin_to_devcimcal(inp):
+	return int(inp,2)
