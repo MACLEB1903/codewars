f653f8d81a957b2f5fd899c7b25ce6907b54e52e
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-09-29 15:20:52 -0400

    feat: add get_volume_of_cuboid() solution for kata 58261acb22be6e2ed800003a

---
+++ python/8kyu/volume_of_a_cuboid.py
@@ -0,0 +1,18 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/python
+
+
+Instructions:
+Bob needs a fast way to calculate the volume of a rectangular cuboid with three values: the length, width and height of the cuboid.
+
+Write a function to help Bob with this calculation.
+
+"""
+
+def get_volume_of_cuboid(length, width, height):
+    return length * width * height
