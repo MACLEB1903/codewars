ef0e1e9284f5c2ae27c3f3a0f84c3987fb3585f2
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-10-09 09:13:55 -0400

    feat: add get_ascii() solution for kata 55acfc59c3c23d230f00006d

---
+++ python/8kyu/get_ascii_value_of_character.py
@@ -0,0 +1,18 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/55acfc59c3c23d230f00006d/train/python
+
+
+Instructions:
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+
+"""
+
+def get_ascii(c):
+	return ord(c)
