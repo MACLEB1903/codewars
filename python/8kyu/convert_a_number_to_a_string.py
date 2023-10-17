655c55bce7e782db139d2e394d52c90b7ddbd2c6
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-10-17 17:07:30 -0400

    feat: add number_to_string() solution for kata 5265326f5fda8eb1160004c8

---
+++ python/8kyu/convert_a_number_to_a_string.py
@@ -0,0 +1,23 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/python
+
+
+Instructions:
+We need a function that can transform a number (integer) into a string.
+What ways of achieving this do you know?
+
+Examples (input --> output):
+
+123  --> "123"
+999  --> "999"
+100 --> "-100"
+
+"""
+
+def number_to_string(num):
+ 	  return str(num)
