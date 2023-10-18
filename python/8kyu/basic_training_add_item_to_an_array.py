f31e52e7ebb825f3071fdaa613fbd90eefc884cc
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-10-18 18:55:50 -0400

    feat: add basic_training_add_item_to_an_array.py solution for kata 511f0fe64ae8683297000001

---
+++ python/8kyu/basic_training_add_item_to_an_array.py
@@ -0,0 +1,20 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/511f0fe64ae8683297000001/train/python
+
+
+Instructions:
+Add the value "codewars" to the websites array.  
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+websites = []
+
+"""
+
+
+websites.append("codewars")
