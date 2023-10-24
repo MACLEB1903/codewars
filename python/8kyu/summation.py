f17c2467d873d0c7a89ff3dce9a97c11da011964
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-10-24 16:22:37 -0400

    add: summation() solution for kata 5966fa4644a98063d50000b7

---
+++ python/8kyu/summation.py
@@ -0,0 +1,18 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5966fa4644a98063d50000b7/train/python
+
+
+Instructions:
+Given an integer, n, find a sum from 1 to n.
+
+For example, sum(4) = 1+2+3+4
+
+"""
+
+def summation(num):
+	return (1 + num) * num / 2
