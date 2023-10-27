5b9a24bf2eb54fa630855dc94bf31e0b1e7ecbba
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-10-27 19:39:49 -0400

    feat: add greet() solution for kata 55a70521798b14d4750000a4

---
+++ python/8kyu/returning_strings.py
@@ -0,0 +1,16 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/55a70521798b14d4750000a4/train/python
+
+
+Instructions:
+[Make sure you type the exact thing I wrote or the program may not execute properly]*
+
+"""
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
