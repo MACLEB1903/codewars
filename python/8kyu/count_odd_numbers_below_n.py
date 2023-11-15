ff148547db51d4a1ec84af7bfa8252c89e26f0c1
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-11-15 16:16:12 -0400

    feat: add odd_count() solution for kata 59342039eb450e39970000a6

---
+++ python/8kyu/count_odd_numbers_below_n.py
@@ -0,0 +1,22 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/59342039eb450e39970000a6/train/python
+
+
+Instructions:
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+Examples:
+7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+
+Expect large Inputs!
+
+"""
+
+def odd_count(n):
+	return n//2
