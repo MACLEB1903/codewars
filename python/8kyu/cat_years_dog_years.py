49308b06c6e45580f8c2220cbb2121714927a932
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-11-16 15:33:34 -0400

    feat: add human_years_cat_years_dog_years() solution for kata 5a6663e9fd56cb5ab800008b

---
+++ python/8kyu/cat_years_dog_years.py
@@ -0,0 +1,33 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/python
+
+
+Instructions:
+I have a cat and a dog.
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+humanYears >= 1
+humanYears are whole numbers only
+
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+
+"""
+
+def human_years_cat_years_dog_years(x):
+    return [x, 24+(x-2)*4 if (x != 1) else 15, 24+(x-2)*5 if (x != 1) else 15]
