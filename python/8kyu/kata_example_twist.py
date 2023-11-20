5b5fcc85d4036f10d0bdb44deec8133ff046f026
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-11-20 10:47:17 -0400

    feat: add kata_example_twist.py solution for kata 525c1a07bb6dda6944000031

---
+++ python/8kyu/kata_example_twist.py
@@ -0,0 +1,18 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/python
+
+
+Instructions:
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata). 
+
+Add the value "codewars" to the array websites 1,000 times.
+
+"""
+
+
+websites = ["codewars"] * 1000
