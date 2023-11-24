239f81205f2ebd2db260656f6db1b276149224dc
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-11-24 14:44:05 -0400

    feat: add classic_hello_world.py solution for kata 57036f007fd72e3b77000023

---
+++ python/8kyu/classic_hello_world.py
@@ -0,0 +1,22 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57036f007fd72e3b77000023/train/python
+
+
+Instructions:
+You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+
+Note that for some languages, the function main is the entry point of the program.
+
+Solution.main("parameter1", "parameter2", "parametern")
+
+"""
+
+
+class Solution:
+    def main(self, *args):
+        print("Hello World!")        
