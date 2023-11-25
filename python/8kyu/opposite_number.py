6bf2ce00d08eefa45a27336976385af237336b0e
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-11-25 16:21:38 -0400

    add: opposite() solution for kata 56dec885c54a926dcd001095

---
+++ python/8kyu/opposite_number.py
@@ -0,0 +1,23 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56dec885c54a926dcd001095/train/python
+
+
+Instructions:
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+1: -1
+14: -14
+34: 34
+
+
+
+"""
+
+def opposite(number):
+    return  - number
