f5bac2f917f3db7996d8d0a2fd71cd9028b87d45
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-12-09 22:24:22 -0400

    feat: add distinct() solution for kata 57a5b0dfcf1fa526bb000118

---
+++ python/8kyu/remove_duplicates_from_list.py
@@ -0,0 +1,23 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/train/python
+
+
+Instructions:
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+"""
+
+def distinct(seq):
+    return sorted(set(seq), key = seq.index)
