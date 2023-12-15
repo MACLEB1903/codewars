8d96efe561d622263af21be173438ad45183074f
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-12-15 13:59:41 -0400

    feat: add get_age() solution for kata 5866fc43395d9138a7000006

---
+++ python/8kyu/ensure_question.py
@@ -0,0 +1,20 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5866fc43395d9138a7000006/train/python
+
+
+Instructions:
+Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+For example (Input --> Output):
+"Yes" --> "Yes?" 
+"No?" --> "No?"
+
+"""
+
+def get_age(age):
+	return int(age[0])
