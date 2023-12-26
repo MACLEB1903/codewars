48ce96577560934ef2b4807d39484e1405f53acc
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2023-12-26 15:51:11 -0400

    feat: add number_to_string.py solution for kata 590889a3a9ded8f49e0011c3

---
+++ python/8kyu/number_to_string.py
@@ -0,0 +1,17 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/590889a3a9ded8f49e0011c3/train/python
+
+
+Instructions:
+Given a double n, return a string that corresponds to the value of n.
+For example, n = 3 should return "3", n = 1e3 should return "1000", n = 2.1 should return "2.1" not 2.100000000. That's as simple as it gets!
+
+"""
+
+
+a = str(123)
