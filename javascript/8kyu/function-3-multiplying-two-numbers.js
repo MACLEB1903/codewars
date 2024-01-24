236deb6f8d25b5d0854026ff7fcce52ad0662a77
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-01-24 15:35:54 -0400

    feat: add function-3-multiplying-two-numbers.js solution for kata 523b66342d0c301ae400003b

---
+++ javascript/8kyu/function-3-multiplying-two-numbers.js
@@ -0,0 +1,15 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+
+
+Instructions:
+This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+
+*/
+
+let multiply = (a, b) => a * b;
