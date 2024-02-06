daa59bb4318db9030a883d87b59496e001d0d587
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-02-06 16:04:37 -0400

    add: capitalizeEachWord() solution for kata 58aac690d68a9c7a300000c0

---
+++ javascript/8kyu/beginner-friendly-uppercase-a-string.js
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/58aac690d68a9c7a300000c0/train/javascript
+
+
+Instructions:
+Convert a string to all uppercase letters.
+
+Example: "Hello world!" to "HELLO WORLD!"
+
+*/
+
+function capitalizeEachWord(str) {
+  return str.toUpperCase();
+}
