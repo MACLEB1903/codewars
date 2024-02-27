0c130c063aaa5478623344c8772105988af0a915
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-02-27 16:27:43 -0400

    add: promiseHelloWorld() solution for kata 5b61d6ef07a266d40b000097

---
+++ javascript/8kyu/a-promise-is-a-promise.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5b61d6ef07a266d40b000097/train/javascript
+
+
+Instructions:
+Create a resolved javascript Promise that will return 'Hello World!'.
+
+*/
+
+async function promiseHelloWorld() {
+  return await "Hello World!";
+}
