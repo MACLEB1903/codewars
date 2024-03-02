d22f7532762c47568fc1392947a2806aab263b25
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-03-02 16:20:16 -0400

    add: binToDec() solution for kata 57a5c31ce298a7e6b7000334

---
+++ javascript/8kyu/bin-to-decimal.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/javascript
+
+
+Instructions:
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+*/
+
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
