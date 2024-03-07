3e749ebe27a03331a05b2b05a4b5f46323c153a4
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-03-07 10:57:49 -0400

    feat: add lyricSplit() solution for kata 5963b6f8a7000805ce000064

---
+++ javascript/8kyu/split-the-lyrics.js
@@ -0,0 +1,21 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5963b6f8a7000805ce000064/train/javascript
+
+
+Instructions:
+Take a lyric that will be input into the function and split the words up into individual elements in a string.
+
+Examples:
+lyricSplit("There's no such thing as a life that's better than yours") // => ["There's", "no", "such", "thing", "as", "a", "life" ,"that's" ,"better", "than", "yours"]
+lyricSplit("I know you still think about the times we had") // => ["I", "know", "you", "still", "think", "about", "the", "times", "we", "had"];
+
+*/
+
+function lyricSplit(lyric) {
+  return lyric.split(` `);
+}
