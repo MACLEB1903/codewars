db04ac9a87a9762d415c3bab0bda0cf3e88eaf22
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-03-12 22:30:06 -0400

    feat: add mystery() solution for kata 514a7ac1a33775cbb500001e

---
+++ javascript/8kyu/return-to-sanity.js
@@ -0,0 +1,18 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/514a7ac1a33775cbb500001e/train/javascript
+
+
+Instructions:
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+*/
+
+function mystery() {
+  var results = { sanity: "Hello" };
+  return results;
+}
