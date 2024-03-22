86d5942a13bb19a3247cb6898070cbb854274dc3
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-03-22 11:43:30 -0400

    feat: add square() solution for kata 599b23c7d7dd7216640000a6

---
+++ javascript/8kyu/square.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/599b23c7d7dd7216640000a6/train/javascript
+
+
+Instructions:
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+function square(n) {
+  return Math.pow(n, 2);
+}
