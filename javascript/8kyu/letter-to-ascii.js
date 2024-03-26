d8c6dffedb47421b0d7a79f6068242ead673e258
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-03-26 16:25:55 -0400

    add: charToASCII() solution for kata 58dc15eed2ba027579000175

---
+++ javascript/8kyu/letter-to-ascii.js
@@ -0,0 +1,22 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/58dc15eed2ba027579000175/train/javascript
+
+
+Instructions:
+This kata is very simple.
+
+You will be given a character and you will convert it to its corresponding number.
+
+Ex. 'a' = 97
+Here is a link to an ASCII table: http://www.asciitable.com/
+
+*/
+
+function charToASCII(c) {
+  return c.charCodeAt();
+}
