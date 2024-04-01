ae8b36afd82a353db66b98e3f146767d5d4cf327
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-04-01 16:00:21 -0400

    add: caseInSwitch() solution for kata 583c905cb0972b3ad90000ea

---
+++ javascript/8kyu/switch.js
@@ -0,0 +1,22 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/583c905cb0972b3ad90000ea/train/javascript
+
+
+Instructions:
+Write a function caseInSwitch which uses a switch statement to test the variable val and sets the variable answer according to the following values of val: 1 - "alpha" 2 - "beta" 3 - "gamma" 4 - "delta"
+
+Then, test caseInSwitch for the argument 1 and print the value of answer to the console.
+
+*/
+
+function caseInSwitch(val) {
+  if (val === 1) return "alpha";
+  if (val === 2) return "beta";
+  if (val === 3) return "gamma";
+  if (val === 4) return "delta";
+}
