2b0b909fa1bf6d4490d6595554fcd16fa5aa686a
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-04-14 16:17:55 -0400

    add: opposite() solution for kata 56dec885c54a926dcd001095

---
+++ javascript/8kyu/opposite-number.js
@@ -0,0 +1,22 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+
+Instructions:
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+1: -1
+14: -14
+-34: 34
+
+*/
+
+function opposite(number) {
+  return -number;
+}
