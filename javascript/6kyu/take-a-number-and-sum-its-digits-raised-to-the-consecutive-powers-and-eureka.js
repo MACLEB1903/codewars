bb19332d86cb564e5e5831ec34bf13ca6e902689
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-04-16 22:31:00 -0400

    feat: add sumDigPow() to find sum of digits raised to their positions

---
+++ javascript/6kyu/take-a-number-and-sum-its-digits-raised-to-the-consecutive-powers-and-eureka.js
@@ -0,0 +1,26 @@
+/*
+
+Examples
+Let's see some cases (input -> output):
+
+1, 10  --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+1, 100 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+
+90, 100 --> []
+
+*/
+
+function sumDigPow(a, b) {
+  let array = [];
+
+  for (a; a <= b; a++) {
+    let total = [...String(a)].reduce(
+      (sum, n, i) => (sum += Math.pow(+n, i + 1)),
+      0
+    );
+    if (total === a) {
+      array.push(a);
+    }
+  }
+  return array;
+}
