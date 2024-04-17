32b32566ff07c68b7c02098c44c1d819fe9dd4a4
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-04-16 21:00:20 -0400

    feat: add hearts() solution for kata 58dbd0162ee7fcc74c0000da

---
+++ javascript/8kyu/write-words-with-love.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/58dbd0162ee7fcc74c0000da/train/javascript
+
+
+Instructions:
+Fix the code, so any written string put in function returns enclosed in two hearts. <3 Please give string a bit of space before and after a heart! <3
+
+*/
+
+function hearts(n) {
+  return `<3 ${n} <3`;
+}
