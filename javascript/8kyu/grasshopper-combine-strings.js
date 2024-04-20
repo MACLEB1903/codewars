b333045396b7f37f2efe2bd1457840d35bc0dd6e
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-04-19 22:10:08 -0400

    feat: add combineNames() solution for kata 55f73f66d160f1f1db000059

---
+++ javascript/8kyu/grasshopper-combine-strings.js
@@ -0,0 +1,24 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/55f73f66d160f1f1db000059/train/javascript
+
+
+Instructions:
+Combine strings function
+Create a function named combineNames(combine_names in python, ruby) that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+combineNames('James', 'Stevens')
+
+returns:
+'James Stevens'
+
+*/
+
+function combineNames(n1, n2) {
+  return `${n1} ${n2}`;
+}
