f29f494b4f2ae40331aba51fd594670ce56d08da
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-04-20 11:20:18 -0400

    feat: add checkType() solution for kata 5799a2d13467dbc64900094b

---
+++ javascript/8kyu/who-am-i.js
@@ -0,0 +1,21 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5799a2d13467dbc64900094b/train/javascript
+
+
+Instructions:
+What type of arguments?
+Write a function that returns the type of its input.
+
+Example
+checkType("aaa") = string
+
+*/
+
+function checkType(value) {
+  return typeof value;
+}
