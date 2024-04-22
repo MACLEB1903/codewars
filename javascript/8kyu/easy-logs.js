d0293a68c02c4f0531ec97593e750e34d384709f
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-04-22 19:09:55 -0400

    feat: add logs() solution for kata 5b68c7029756802aa2000176

---
+++ javascript/8kyu/easy-logs.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5b68c7029756802aa2000176/train/javascript
+
+
+Instructions:
+Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+
+*/
+
+function logs(x, a, b) {
+  return Math.log(a * b) / Math.log(x);
+}
