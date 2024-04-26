cd577024a607eded1673a81f75cba8b9bf716087
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-04-26 16:22:46 -0400

    add: convertLowerCase() solution for kata 58aacf3e048560c705000799

---
+++ javascript/8kyu/beginner-friendly-lowercase-letters.js
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/58aacf3e048560c705000799/train/javascript
+
+
+Instructions:
+Convert a string to lowercase letters.
+
+Example: convert "Hello world!" to "hello world!"
+
+*/
+
+function convertLowerCase(str) {
+  return str.toLowerCase();
+}
