469d63110b89a336789f54c384e6203e6b2c0216
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-05-12 16:13:07 -0400

    add: boolToWord() solution for kata 53369039d7ab3ac506000467

---
+++ javascript/8kyu/convert-boolean-values-to-strings-yes-or-no.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+
+Instructions:
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+*/
+
+function boolToWord(bool) {
+  return bool ? "Yes" : "No";
+}
