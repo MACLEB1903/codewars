82efb33f738dbe6841e3f84aafff1eae3ee58eb5
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-06-01 19:12:43 -0400

    feat: add alphabetti() to return string in alphabetical order

---
+++ javascript/8kyu/alphabetti-spaghetti.js
@@ -0,0 +1,10 @@
+/*
+
+Description:
+You will be given a string of letters. The string must be returned in alphabetical order.
+
+*/
+
+function alphabetti(str) {
+  return [...str.toLowerCase()].sort();
+}
