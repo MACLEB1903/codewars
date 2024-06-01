6e89c01967f5993073f5e082267ba3aff3655ee6
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-06-01 16:10:29 -0400

    add: function-2-squaring-an-argument solution for kata 523b623152af8a30c6000027

---
+++ javascript/8kyu/function-2-squaring-an-argument.js
@@ -0,0 +1,15 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+
+
+Instructions:
+Now you have to write a function called square that takes an argument and returns the square of it.
+
+*/
+
+const square = (a) => Math.pow(a, 2);
