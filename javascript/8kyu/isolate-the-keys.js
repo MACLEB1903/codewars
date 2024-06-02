9adbb53b1901d825c05f7aa4bbaebd93bb525036
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-06-02 16:07:40 -0400

    add: getPropsFromObj() solution for kata 573b4dbdffc3d1a51800137f

---
+++ javascript/8kyu/isolate-the-keys.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/573b4dbdffc3d1a51800137f/train/javascript
+
+
+Instructions:
+Take in an Hash and return an Array filled with each key. Be creative
+
+*/
+
+function getPropsFromObj(obj) {
+  return Object.keys(obj);
+}
