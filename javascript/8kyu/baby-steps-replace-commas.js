b73339f8d2f5fa049f57ee91c8a6006089350b93
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-07-03 16:01:46 -0400

    add: commaReplacer() solution for kata 56895d1636919c5609000059

---
+++ javascript/8kyu/baby-steps-replace-commas.js
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56895d1636919c5609000059/train/javascript
+
+
+Instructions:
+Jon has a string but there are commas (,) where there should be spaces (' ').
+
+Your task is to help Jon replace the commas with spaces.
+
+*/
+
+function commaReplacer(str) {
+  return str.replace(/,/g, " ");
+}
