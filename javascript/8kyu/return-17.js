84a5a11cdff2b471527fb8b60706807da2f9ea35
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-07-09 16:28:13 -0400

    add: return-17 solution for kata 57f022a6cba9da84a3000095

---
+++ javascript/8kyu/return-17.js
@@ -0,0 +1,23 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57f022a6cba9da84a3000095/train/javascript
+
+
+Instructions:
+You are given a function named return17.
+
+You should fix the if expression so the function will return 17, by making exactly two modifications: add one character and remove one character.
+
+Good luck!
+
+*/
+
+var return17 = function () {
+  var a = 1;
+  if (a == 5000) return 13;
+  return 17;
+};
