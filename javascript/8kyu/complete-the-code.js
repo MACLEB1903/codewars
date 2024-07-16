6462582ff144776e905d033b648910e5287726df
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-07-16 12:27:14 -0400

    feat: add canYouDrive() solution for kata 59c265c39f0cbc7cae000093

---
+++ javascript/8kyu/complete-the-code.js
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/59c265c39f0cbc7cae000093/train/javascript
+
+
+Instructions:
+The age of driving is 18 at minimum.
+
+Create a function that checks 'age' value and return true if it is 18 or bigger If not, return false.
+
+*/
+
+function canYouDrive(age) {
+  return age >= 18;
+}
