f82a9fe862624c2e991587a995113fdda40e360f
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-07-17 16:14:44 -0400

    add: removeEveryOther() solution for kata 5769b3802ae6f8e4890009d2

---
+++ javascript/8kyu/removing-elements.js
@@ -0,0 +1,22 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/javascript
+
+
+Instructions:
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+
+*/
+
+function removeEveryOther(arr){
+  return arr.filter((_, i) =>  ((i+1)%2) === 1)
+}
