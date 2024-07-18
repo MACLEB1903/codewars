42af23f3a64fc460cc7eaab9983a42de5c6bb2c7
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-07-18 16:00:24 -0400

    add: intToBin() solution for kata 55fc061dfb8005f480000051

---
+++ javascript/8kyu/convert-natural-number-to-its-binary-representation.js
@@ -0,0 +1,23 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/55fc061dfb8005f480000051/train/javascript
+
+
+Instructions:
+Write function that converts an integer number to its binary representation and returns it as a string.
+
+You can see examples of how function should work in test cases.
+
+You can find some useful informations under this link: https://en.wikipedia.org/wiki/Binary_number.
+
+Input numbers will be greater than or equal to 0.
+
+*/
+
+function intToBin(number) {
+  return number.toString(2);
+}
