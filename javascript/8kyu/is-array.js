c0700ff2015226393399de0c29995733b4a2a1f3
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-07-19 12:59:33 -0400

    feat: add isArray() to check if a variable is an array

---
+++ javascript/8kyu/is-array.js
@@ -0,0 +1,16 @@
+/*
+
+Description:
+Create a function that returns if the variable is an array, i.e.
+
+isArray([]) => true
+isArray({}) => false
+isArray('lero') => false
+isArray('') => false
+isArray([0]) => true
+
+*/
+
+function isArray(value) {
+  return Array.isArray(value);
+}
