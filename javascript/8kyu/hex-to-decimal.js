a349dc78c3b63983a126c3732f5b12a0085ee344
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-08-13 16:05:05 -0400

    add: hexToDec() solution for kata 57a4d500e298a7952100035d

---
+++ javascript/8kyu/hex-to-decimal.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57a4d500e298a7952100035d/train/javascript
+
+
+Instructions:
+Complete the function which converts hex number (given as a string) to a decimal number.
+
+*/
+
+function hexToDec(hexString) {
+  return parseInt(hexString, 16);
+}
