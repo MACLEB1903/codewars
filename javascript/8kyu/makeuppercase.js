bf7b434e3e101f926c944d54739beb629556f943
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-08-18 16:13:06 -0400

    add: makeUpperCase() solution for kata 57a0556c7cb1f31ab3000ad7

---
+++ javascript/8kyu/makeuppercase.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+
+
+Instructions:
+Write function makeUpperCase.
+
+*/
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
