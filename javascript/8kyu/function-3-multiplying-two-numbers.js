d22cff0afa06b0a212b9ab277e7c4274a6581b9b
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-08-21 15:24:36 -0400

    feat: add multiply() to return the product of two numbers

---
+++ javascript/8kyu/function-3-multiplying-two-numbers.js
@@ -0,0 +1,8 @@
+/*
+
+Description:
+This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+
+*/
+
+let multiply = (a, b) => a * b;
