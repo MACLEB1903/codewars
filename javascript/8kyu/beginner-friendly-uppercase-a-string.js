248209df93b2b8edeb89597be22d2e4d713db06d
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-08-25 11:01:42 -0400

    feat: add capitalizeEachWord() to convert string to uppercase letters

---
+++ javascript/8kyu/beginner-friendly-uppercase-a-string.js
@@ -0,0 +1,12 @@
+/*
+
+Description:
+Convert a string to all uppercase letters.
+
+Example: "Hello world!" to "HELLO WORLD!"
+
+*/
+
+function capitalizeEachWord(str) {
+  return str.toUpperCase();
+}
