d2106f269d6a5c5eb42f1156b7e77cfda88fd122
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-08-28 16:07:00 -0400

    add: switch-it-up solution for kata 5808dcb8f0ed42ae34000031

---
+++ javascript/8kyu/switch-it-up.js
@@ -0,0 +1,33 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/javascript
+
+
+Instructions:
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+Output :: "One".
+
+Try using "Switch" statements.
+This kata is meant for beginners. Rank and upvote to bring it out of beta
+
+*/
+
+switchItUp = (n) =>
+  [
+    "Zero",
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+  ][n];
