8c2656974121656bc46633ba9be6962cf2ae0afa
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-09-15 16:12:25 -0400

    add: babyRepeat() solution for kata 5a216a2dee1aae8e3e00001a

---
+++ javascript/8kyu/baby-repeat.js
@@ -0,0 +1,21 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5a216a2dee1aae8e3e00001a/train/javascript
+
+
+Instructions:
+Baby is just learning to talk, and is starting out with easy common everyday words. You want to encourage your baby to talk more, so you repeat the random words they learned. When baby says a word, you repeat the word twice back at baby. Write a function to illustrate this exercise.
+
+ie: 
+baby says("yum");
+you say("yum yum");
+
+*/
+
+function babyRepeat(babble) {
+  return babble.repeat(2);
+}
