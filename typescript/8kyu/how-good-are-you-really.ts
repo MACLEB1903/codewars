b1e1de46ae359f4a2dc3fca4f9546dda507c79ab
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-09-20 19:42:40 -0400

    feat: add betterThanAverage() solution for kata 5601409514fc93442500010b

---
+++ typescript/8kyu/how-good-are-you-really.ts
@@ -0,0 +1,28 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5601409514fc93442500010b/train/typescript
+
+
+Instructions:
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+Return true if you're better, else false!
+
+Note:
+Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+*/
+
+export function betterThanAverage(
+  classPoints: number[],
+  yourPoints: number
+): boolean {
+  return (
+    classPoints.reduce((sum, n) => (sum += n)) / classPoints.length < yourPoints
+  );
+}
