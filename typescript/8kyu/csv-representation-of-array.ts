de4df8ee4ee179aaeeedfa28cd53fc8d91b3be44
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-09-23 15:01:11 -0400

    feat: add toCsvText() solution for kata 5a34af40e1ce0eb1f5000036

---
+++ typescript/8kyu/csv-representation-of-array.ts
@@ -0,0 +1,34 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036/train/typescript
+
+
+Instructions:
+Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+Example:
+
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]] 
+
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+Array's length > 2.
+
+More details here: https://en.wikipedia.org/wiki/Comma-separated_values
+
+*/
+
+export function toCsvText(array: number[][]): string {
+  return array.map(row => row.join(",")).join("\n");
+}
