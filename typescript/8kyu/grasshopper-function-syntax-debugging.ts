fd2fdc79dc5bb9e0ff6de17d6ea38c9280528706
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-09-24 16:03:17 -0400

    add: main() solution for kata 56dae9dc54c0acd29d00109a

---
+++ typescript/8kyu/grasshopper-function-syntax-debugging.ts
@@ -0,0 +1,18 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/typescript
+
+
+Instructions:
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+*/
+
+function main(verb: string, noun: string): string {
+  return `${verb}${noun}`;
+}
