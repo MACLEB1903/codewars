25e13233fa83484b9658eae7fd1eb3b365eb7ab5
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-10-04 11:04:02 -0400

    feat: add convert-boolean-values-to-strings-yes-or-no.ts solution for kata 53369039d7ab3ac506000467

---
+++ typescript/8kyu/convert-boolean-values-to-strings-yes-or-no.ts
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/53369039d7ab3ac506000467/train/typescript
+
+
+Instructions:
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+*/
+
+export const boolToWord = (bool: boolean): string => {
+    return bool ? "Yes" : "No";
+  };
