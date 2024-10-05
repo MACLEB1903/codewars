70ce861f49cb17346756ba43449b98c86e62874a
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-10-05 16:04:50 -0400

    add: check() solution for kata 57cc40b2f8392dbf2a0003ce

---
+++ typescript/8kyu/no-loops-2-you-only-need-one.ts
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57cc40b2f8392dbf2a0003ce/train/typescript
+
+
+Instructions:
+No Loops Allowed
+You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+*/
+
+export function check(a: number[], x: number): boolean {
+  return a.includes(x);
+}
