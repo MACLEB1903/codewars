3f5080090f5a57135fc72b0c02e9d8b5ff512005
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-11-12 18:42:35 -0400

    feat: add rez() solution for kata 55e56e5b2f71cee88f00004f

---
+++ typescript/8kyu/change-for-pizza.ts
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/55e56e5b2f71cee88f00004f/train/typescript
+
+
+Instructions:
+My friend Sergio visits a pizzeria every Tuesday and buys a pizza. Sergio always gives a 200 hryvnias banknote, but the pizza's price is always less than 200 hryvnias.
+
+The prices sometimes changes and the pizzeria needs a special program to calculate the amount of money they need to return. The pizzeria's owner studied qbasic in college so he wrote javascript as qbasic. Please, help him and fix a function that calculates the change for pizza.
+
+*/
+
+export function rez(price: number): number {
+  return 200 - price;
+}
