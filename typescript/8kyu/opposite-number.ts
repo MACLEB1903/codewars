64eb0d954adbf622c6d8aa095959606a27c5a468
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-11-13 18:22:27 -0400

    feat: add opposite-number.ts solution for kata 56dec885c54a926dcd001095

---
+++ typescript/8kyu/opposite-number.ts
@@ -0,0 +1,20 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56dec885c54a926dcd001095/train/typescript
+
+
+Instructions:
+Very simple, given a number, find its opposite.
+1: -1
+
+*/
+
+export class Kata {
+  static opposite(n: number) {
+    return -n;
+  }
+}
