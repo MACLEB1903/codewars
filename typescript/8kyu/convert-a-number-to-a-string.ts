44dfd41d03f2fdb8fcb5c8c7afe04aa6ba524cb5
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-11-17 16:06:31 -0400

    add: numberToString() solution for kata 5265326f5fda8eb1160004c8

---
+++ typescript/8kyu/convert-a-number-to-a-string.ts
@@ -0,0 +1,23 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/typescript
+
+
+Instructions:
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+numberToString(123); // returns '123';`   
+numberToString(999); // returns '999';`
+
+*/
+
+export function numberToString(num: number): string {
+  return num.toString();
+}
