cda974c5daa9f9565e477c6c25812a0f3f19c532
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-11-20 16:02:48 -0400

    add: reversed-strings solution for kata 5168bb5dfe9a00b126000018

---
+++ typescript/8kyu/reversed-strings.ts
@@ -0,0 +1,15 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/typescript
+
+
+Instructions:
+Complete the solution so that it reverses the string passed into it.
+
+*/
+
+export const solution = (str: string): string => [...str].reverse().join('');
