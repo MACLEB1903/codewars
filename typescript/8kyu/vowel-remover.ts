17356bf8392ff498b6a2f6d29f9b7a88c5cf572b
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-12-03 14:44:49 -0400

    feat: add shortcut() solution for kata 5547929140907378f9000039

---
+++ typescript/8kyu/vowel-remover.ts
@@ -0,0 +1,22 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5547929140907378f9000039/train/typescript
+
+
+Instructions:
+Create a function called shortcut to remove all the lowercase vowels in a given string.
+
+Examples:
+shortcut("codewars") // --> cdwrs
+shortcut("goodbye")  // --> gdby
+Don't worry about uppercase vowels.
+
+*/
+
+export function shortcut(string: string): string {
+  return string.replace(/[aeiou]/g, "");
+}
