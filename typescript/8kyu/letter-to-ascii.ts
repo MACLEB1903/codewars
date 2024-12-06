28ef16a05f854536c4e62b4afdb0997092252b62
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-12-06 14:05:53 -0400

    feat: add charToASCII() solution for kata 58dc15eed2ba027579000175

---
+++ typescript/8kyu/letter-to-ascii.ts
@@ -0,0 +1,23 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/58dc15eed2ba027579000175/train/typescript
+
+
+Instructions:
+This kata is very simple.
+
+You will be given a character and you will convert it to its corresponding number.
+
+Ex. 'a' = 97
+
+Here is a link to an ASCII table: http://www.asciitable.com/
+
+*/
+
+export function charToASCII(c: string): number {
+  return c.charCodeAt(0);
+}
