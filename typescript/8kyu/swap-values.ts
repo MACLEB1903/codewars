741437c9a070f34fffe2864e0002a0d7a9bf3717
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-12-12 16:06:48 -0400

    add: swapValues() solution for kata 5388f0e00b24c5635e000fc6

---
+++ typescript/8kyu/swap-values.ts
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5388f0e00b24c5635e000fc6/train/typescript
+
+
+Instructions:
+I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+Can you figure out what's wrong here?
+
+*/
+
+export function swapValues(arr: any[]): any {
+  return arr.reverse();
+}
