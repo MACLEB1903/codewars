ae2fcbf679d2b90ba7dfdaed12c560c3b52c13cc
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-12-22 12:38:20 -0400

    feat: add checkTheBucket() solution for kata 59bd5dc270a3b7350c00008b

---
+++ typescript/8kyu/man-in-the-west.ts
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/59bd5dc270a3b7350c00008b/train/typescript
+
+
+Instructions:
+A western man is trying to find gold in a river. To do that, he passes a bucket through the river's soil and then checks if it contains any gold. However, he could be more productive if he wrote an algorithm to do the job for him.
+
+So, you need to check if there is gold in the bucket, and if so, return True/true. If not, return False/false.
+
+*/
+
+export function checkTheBucket(bucket: string[]): boolean {
+  return bucket.includes("gold");
+}
