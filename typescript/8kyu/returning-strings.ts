31f21ff7337217ee71b9c9fc855501c0604991f2
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-01-05 16:27:14 -0400

    add: returning-strings solution for kata 55a70521798b14d4750000a4

---
+++ typescript/8kyu/returning-strings.ts
@@ -0,0 +1,16 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/55a70521798b14d4750000a4/train/typescript
+
+
+Instructions:
+Make a function that will return a greeting statement that uses an input; 
+your program should return, "Hello, <name> how are you doing today?".
+
+*/
+
+export const greet = (name: string): string => `Hello, ${name} how are you doing today?`;
