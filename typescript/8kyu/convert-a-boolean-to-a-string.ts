97c5e3fb7823ed9bab6756df770cc23c9be7a8fb
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-01-18 14:48:59 -0400

    feat: add booleanToString() solution for kata 551b4501ac0447318f0009cd

---
+++ typescript/8kyu/convert-a-boolean-to-a-string.ts
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/typescript
+
+
+Instructions:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+export function booleanToString(b: boolean): string {
+  return `${b}`
+}
