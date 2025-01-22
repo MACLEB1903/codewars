5474367e442d3baf14f1ed6cf493c4abfef00389
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-01-22 16:29:02 -0400

    add: square solution for kata 599b23c7d7dd7216640000a6

---
+++ typescript/8kyu/square.ts
@@ -0,0 +1,15 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/599b23c7d7dd7216640000a6/train/typescript
+
+
+Instructions:
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+export const square = (n: number) => Math.pow(n, 2);
