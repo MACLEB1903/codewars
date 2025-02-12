8e83ad65beb3c89348dac7b35b1ddde69b5c501d
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-02-12 19:14:04 -0400

    feat: string.prototype.toalternatingcase() - convert string to alternating case

--- 8kyu/alternating-case.js
+++ 8kyu/alternating-case.js
@@ -12,3 +12,9 @@ Define String.prototype.toAlternatingCase (or a similar function/method such as 
 As usual, your function/method should be pure, i.e. it should not mutate the original string.
 
 */
+
+String.prototype.toalternatingcase = function () {
+  return [...this]
+    .map((l) => (l === l.toUpperCase() ? l.toLowerCase() : l.toUpperCase()))
+    .join("");
+};
