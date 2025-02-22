11e250b45c17ebaf2d955b5e340c7983908ea858
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-02-22 19:00:08 -0400

    feat: add canYouDrive() solution for kata 59c265c39f0cbc7cae000093

---
+++ typescript/8kyu/complete-the-code.ts
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/59c265c39f0cbc7cae000093/train/typescript
+
+
+Instructions:
+The age of driving is 18 at minimum.
+
+Create a function that checks 'age' value and return true if it is 18 or bigger If not, return false.
+
+*/
+
+export function canYouDrive(age: number): boolean {
+  return age >= 18;
+}
