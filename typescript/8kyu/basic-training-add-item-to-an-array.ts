cb2bc4ff2e27dc6f26f4d092ca255c7c6d674fb3
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-02-28 19:16:51 -0400

    feat: add basic-training-add-item-to-an-array.ts solution for kata 511f0fe64ae8683297000001

---
+++ typescript/8kyu/basic-training-add-item-to-an-array.ts
@@ -0,0 +1,21 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/511f0fe64ae8683297000001/train/typescript
+
+
+Instructions:
+Add the value "codewars" to the websites array.
+After your code executes the websites array should?: any = = ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+*/
+
+var websites: string[] = [];
+websites.push("codewars");
