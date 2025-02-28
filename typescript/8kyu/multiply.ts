7ad6de559dc7e3f81ba713d2b1866845713f5433
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-02-28 16:06:57 -0400

    add: multiply() solution for kata 50654ddff44f800200000004

---
+++ typescript/8kyu/multiply.ts
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/50654ddff44f800200000004/train/typescript
+
+
+Instructions:
+The code does not execute properly. Try to figure out why.
+
+*/
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
