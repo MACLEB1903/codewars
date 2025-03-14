1202415cdf254bac41ad7ae53fca67c9ba731ac9
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-03-14 16:19:16 -0400

    add: makeUpperCase() solution for kata 57a0556c7cb1f31ab3000ad7

---
+++ typescript/8kyu/makeuppercase.ts
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/typescript
+
+
+Instructions:
+Write function makeUpperCase.
+
+*/
+
+export function makeUpperCase(str: string): string {
+  return str.toUpperCase();
+}
