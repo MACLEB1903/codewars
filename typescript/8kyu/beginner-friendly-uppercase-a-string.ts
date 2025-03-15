57763fa511c11717afdc77b7e3b6c14467af45f8
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-03-15 12:05:57 -0400

    feat: add capitalizeEachWord() solution for kata 58aac690d68a9c7a300000c0

---
+++ typescript/8kyu/beginner-friendly-uppercase-a-string.ts
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/58aac690d68a9c7a300000c0/train/typescript
+
+
+Instructions:
+Convert a string to all uppercase letters.
+
+Example: "Hello world!" to "HELLO WORLD!"
+
+*/
+
+export function capitalizeEachWord(str: string): string {
+  return str.toUpperCase();
+}
