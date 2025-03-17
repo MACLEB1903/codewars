02b03e274bdb775a829c09185527705c37c74dc7
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-03-17 16:02:37 -0400

    add: multiply-two-numbers solution for kata 554290d66bd4fb1cd00000d0

---
+++ typescript/8kyu/multiply-two-numbers.ts
@@ -0,0 +1,16 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/554290d66bd4fb1cd00000d0/train/typescript
+
+
+Instructions:
+Implement a function which multiplies two numbers.
+
+*/
+
+export const multiply = (num1: number, num2: number) => num1 * num2;
+
