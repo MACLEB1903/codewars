da085e511f79ffc57af6a1ecb40c3350739bd5b4
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-03-22 09:39:29 -0400

    feat: add mystery() solution for kata 514a7ac1a33775cbb500001e

---
+++ typescript/8kyu/return-to-sanity.ts
@@ -0,0 +1,18 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/514a7ac1a33775cbb500001e/train/typescript
+
+
+Instructions:
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+*/
+
+export function mystery(): {sanity: string} {
+  var results = { sanity: "Hello" };
+  return results;
+}
