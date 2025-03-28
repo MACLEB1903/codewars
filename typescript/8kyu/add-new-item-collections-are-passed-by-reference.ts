69d7bd7bccb6d81db3abe403d341c7c1e842d65c
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-03-28 15:58:24 -0400

    feat: add addExtra() solution for kata 566dc05f855b36a031000048

---
+++ typescript/8kyu/add-new-item-collections-are-passed-by-reference.ts
@@ -0,0 +1,23 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/566dc05f855b36a031000048/train/typescript
+
+
+Instructions:
+Add an item to the list:
+
+AddExtra method adds a new item to the list and returns the list. The new item can be any object, and it does not matter. (lets say you add an integer value, like 13)
+
+In our test case we check to assure that the returned list has one more item than the input list. However the method needs some modification to pass this test.
+
+P.S. You have to create a new list and add a new item to that. (This Kata is originally designed for C# language and it shows that adding a new item to the input list is not going to work, even though the parameter is passed by value, but the value is poining to the reference of list and any change on parameter will be seen by caller)
+
+*/
+
+export function addExtra(listOfNumbers: number[]): number[] {
+  return [...listOfNumbers, 1];
+}
