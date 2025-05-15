d14fbb70c94c0abf0fa7af37c461394f28adc1b0
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-05-15 19:43:05 -0400

    feat: add sayHello() solution for kata 5625618b1fe21ab49f00001f

---
+++ typescript/8kyu/grasshopper-debug-sayhello.ts
@@ -0,0 +1,21 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/typescript
+
+
+Instructions:
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+Hello, Mr. Spock
+
+*/
+
+export function sayHello(name: number): string {
+  return `Hello, ${name}`;
+}
