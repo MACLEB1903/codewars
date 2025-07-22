011747da9d7f6fa8a2ae88225581029ecce65692
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-07-22 19:45:53 -0400

    feat: add is-it-even.ts solution for kata 555a67db74814aa4ee0001b5

---
+++ typescript/8kyu/is-it-even.ts
@@ -0,0 +1,19 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/typescript
+
+
+Instructions:
+In this Kata we are passing a number (n) into a function.
+Your code will determine if the number passed is even (or not).
+The function needs to return either a true or false.
+Numbers may be positive or negative, integers or floats.
+Floats are considered UNeven for this kata.
+
+*/
+
+export const testEven = (n : number) : boolean => n % 2 == 0
