88f08223abed45396f4011e7c97a101ec295e517
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2025-08-23 07:16:42 +0000

    feat: add removeExclamationMarks() solution for kata 57a0885cbb9944e24c00008e

---
+++ javascript/8kyu/remove-exclamation-marks.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/javascript
+
+
+Instructions:
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+*/
+
+function removeExclamationMarks(s) {
+  return s.replaceAll("!", "");
+}
