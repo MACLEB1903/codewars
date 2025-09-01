894f8808b9d30c4d668d7a2b0b1c84b4b52431c4
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2025-09-01 23:28:01 +0000

    feat: add toCamelCase() solution for kata 517abf86da9663f1d2000003

---
+++ typescript/6kyu/convert-string-to-camel-case.ts
@@ -0,0 +1,27 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/517abf86da9663f1d2000003/train/typescript
+
+
+Instructions:
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+
+*/
+
+export function toCamelCase(str:string):string{
+  return str.split(/[^a-z]/gi).map((s,i) => i==0 ? s : s[0].toUpperCase() + s.slice(1)).join("")
+}
+
+export function toCamelCase(str:string):string{
+  return str.replace(/[-_](.)/g, (_,char)=>char.toUpperCase())
+
+}
