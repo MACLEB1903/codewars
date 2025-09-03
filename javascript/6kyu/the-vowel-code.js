639dc742a4ab1621fab72621d8ad3cc4aee8b89f
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2025-09-03 21:01:30 +0000

    feat: add the-vowel-code.js solution for kata 53697be005f803751e0015aa

---
+++ javascript/6kyu/the-vowel-code.js
@@ -0,0 +1,35 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/53697be005f803751e0015aa/train/javascript
+
+
+Instructions:
+Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+
+For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+For example, decode("h3 th2r2") would return "hi there".
+For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+*/
+
+function encode(string) {
+  const vowels = "aeiou";
+  return [...string].map(l => vowels.includes(l) ? vowels.indexOf(l)+1 : l).join("");
+}
+
+function decode(string) {
+   const vowels = "aeiou";
+  return [...string].map(l => Number(l) > 0 ? vowels[Number(l)-1]: l).join("");
+}
