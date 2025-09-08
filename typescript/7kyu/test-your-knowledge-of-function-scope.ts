1d3fcf312803e81dcca33f91ede6e5c55c7051cb
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2025-09-08 19:28:06 +0000

    feat: add add() solution for kata 56d344c7fd3a52566700124b

---
+++ typescript/7kyu/test-your-knowledge-of-function-scope.ts
@@ -0,0 +1,20 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56d344c7fd3a52566700124b/train/typescript
+
+
+Instructions:
+Write a function that adds from two invocations.
+
+All inputs will be integers.
+
+add(3)(4)  // 7
+add(12)(20) // 32
+
+*/
+
+export const add = (x: number) => (y: number): number => x + y;
