f890cf2f21e67098828dcb2f2bbf82ef38b3b8f8
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2025-09-08 19:24:00 +0000

    feat: add add() solution for kata 56d344c7fd3a52566700124b

---
+++ javascript/7kyu/test-your-knowledge-of-function-scope.js
@@ -0,0 +1,26 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56d344c7fd3a52566700124b/train/javascript
+
+
+Instructions:
+Write a function that adds from two invocations.
+
+All inputs will be integers.
+
+add(3)(4)  // 7
+add(12)(20) // 32
+
+*/
+
+const add = (x) => (y) => x+y;
+
+/*
+
+const add = a => b => a+b;
+
+*/
