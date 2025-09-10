f19ec28725b37e7a7efa21492511df9f551c0e72
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2025-09-10 02:53:06 +0000

    feat: add explode() solution for kata 585b1fafe08bae9988000314

---
+++ typescript/7kyu/digits-explosion.ts
@@ -0,0 +1,21 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/585b1fafe08bae9988000314/train/typescript
+
+
+Instructions:
+Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+"312" should return "333122"
+"102269" should return "12222666666999999999"
+
+*/
+
+export function explode(s: string): string {
+  return [...s].map(n => n.repeat(+n)).join("")
+}
