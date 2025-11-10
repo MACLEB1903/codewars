857adb3f798e88a17383bd58bb0dc110549c8e58
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2025-11-10 01:04:48 +0000

    feat: add keep-hydrated.sh solution for kata 582cb0224e56e068d800003c

---
+++ shell/8kyu/keep-hydrated.sh
@@ -0,0 +1,25 @@
+: << "COMMENT"
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/582cb0224e56e068d800003c/train/shell
+
+
+Instructions:
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded down.
+
+For example:
+time = 3 ----> litres = 1
+time = 6.7---> litres = 3
+time = 11.8--> litres = 5
+
+COMMENT
+
+#!/bin/bash
+time=$1
+bc <<< "$time / 2"
