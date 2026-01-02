b20232ab6019464518da971597d5953527384e03
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2026-01-02 14:15:49 +0000

    feat: add say_hello() solution for kata 5302d846be2a9189af0001e4

---
+++ python/8kyu/welcome_to_the_city.py
@@ -0,0 +1,21 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/python
+
+
+Instructions:
+Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+Example:
+
+['John', 'Smith'], 'Phoenix', 'Arizona'
+This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+"""
+
+def say_hello(name, city, state):
+    return f"Hello, {' '.join(name)}! Welcome to {city}, {state}!"
