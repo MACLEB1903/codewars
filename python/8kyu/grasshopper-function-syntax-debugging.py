5936e9ab118d9c0041a111fd5f940377479b0d0f
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2026-01-05 22:15:46 +0000

    feat: add main() solution for kata 56dae9dc54c0acd29d00109a

---
+++ python/8kyu/grasshopper-function-syntax-debugging.py
@@ -0,0 +1,17 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/python
+
+
+Instructions:
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+"""
+
+def main (verb, noun): return verb + noun
+		 
