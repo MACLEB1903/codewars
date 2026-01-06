49287b0d4cb65033e45a57f183e354bf08cf6feb
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2026-01-06 14:25:32 +0000

    feat: add disariumNumber() solution for kata 5a53a17bfd56cb9c14000003

---
+++ typescript/7kyu/disarium-number.ts
@@ -0,0 +1,38 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5a53a17bfd56cb9c14000003/train/typescript
+
+
+Instructions:
+Definition
+Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
+
+Task
+Given a number, Find if it is Disarium or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+
+Notes:
+Number passed is always Positive.
+Return the result as String
+
+Input >> Output Examples
+disariumNumber(89) ==> return "Disarium !!"
+
+Explanation:
+Since , 81 + 92 = 89 , thus output is "Disarium !!"
+disariumNumber(564) ==> return "Not !!"
+
+Explanation:
+Since , 51 + 62 + 43 = 105 != 564 , thus output is "Not !!
+
+*/
+
+export function disariumNumber(n: number): string {
+ return n == [...`$(n)`].reduce((sum, x, i) => sum += Math.pow(+x, i+1), 0) ? "Disarium !!" : "Not !!";
+}
