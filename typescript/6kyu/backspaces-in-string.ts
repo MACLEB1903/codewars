e4b6bdb28456072f4cc88d688ff32ae98cad21b7
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2026-01-10 23:17:25 +0000

    feat: add cleanString() solution for kata 5727bb0fe81185ae62000ae3

---
+++ typescript/6kyu/backspaces-in-string.ts
@@ -0,0 +1,27 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/5727bb0fe81185ae62000ae3/train/typescript
+
+
+Instructions:
+Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+Your task is to process a string with "#" symbols.
+
+Examples:
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+
+*/
+
+export function cleanString(s: string): string {
+	const arr = [];
+	for (const char of [...s]) char == "#" ? arr.pop()  : arr.push(char);
+	
+	return arr.join("");
+}
