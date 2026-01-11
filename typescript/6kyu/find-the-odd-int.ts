e433ce7fe9023ae2133b11160b38fcded0ef0778
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2026-01-11 16:04:13 +0000

    feat: add findOdd() solution for kata 54da5a58ea159efa38000836

---
+++ typescript/6kyu/find-the-odd-int.ts
@@ -0,0 +1,31 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/54da5a58ea159efa38000836/train/typescript
+
+
+Instructions:
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+*/
+
+export const findOdd = (xs: number[]): number => {
+  return [...new Set(xs)].filter(num => xs.filter(n => num == n).length % 2 == 1)[0]
+};
+
+export const findOdd = (xs: number[]): number => {
+  return xs.reduce((a,b) => a^b)
+};
+
