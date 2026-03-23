8b550db53ccd6e03331fd601a5b09c5a824f0e89
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2026-03-23 15:04:45 +0000

    feat: add magic_sum() solution for kata 57193a349906afdf67000f50

---
+++ python/7kyu/magic-sum-of-3s.py
@@ -0,0 +1,19 @@
+"""
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+https://www.codewars.com/kata/57193a349906afdf67000f50/train/python
+
+
+Instructions:
+The magic sum of 3s is calculated on an array by summing up odd numbers which include the digit 3.
+Complete the function which accepts an array of integers and returns its magic sum of 3s.
+
+Example: [3, 12, 5, 8, 30, 13] results in 16 (3 + 13)
+
+If there is no such number in the array, 0 should be returned.
+
+"""
+
+def magic_sum(arr):
+    return sum(list(filter(lambda x: "3" in str(x) and x%2==1,arr)))
