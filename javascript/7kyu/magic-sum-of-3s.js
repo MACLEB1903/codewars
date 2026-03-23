60cdc4d94f1e01feb4f1240c8fc4838c2a433898
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2026-03-23 15:12:16 +0000

    feat: solution for kata magicSum() solution for kata 57193a349906afdf67000f50

---
+++ javascript/7kyu/magic-sum-of-3s.js
@@ -0,0 +1,22 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/57193a349906afdf67000f50/train/javascript
+
+
+Instructions:
+The magic sum of 3s is calculated on an array by summing up odd numbers which include the digit 3.
+Complete the function which accepts an array of integers and returns its magic sum of 3s.
+
+Example: [3, 12, 5, 8, 30, 13] results in 16 (3 + 13)
+
+If there is no such number in the array, 0 should be returned.
+
+*/
+
+function magicSum(numbers) {
+  return numbers.filter(x => x%2 && `${x}`.includes(3)).reduce((sum, n) => sum+=n, 0)
+}
