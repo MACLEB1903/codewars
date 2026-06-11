6b7c149e46d1fb5a9d5e79c89a58dcd9b7752a2a
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2026-06-11 20:23:40 +0000

    feat: add include() solution for kata 545991b4cbae2a5fda000158

---
+++ javascript/8kyu/does-my-list-include-this.js
@@ -0,0 +1,17 @@
+/*
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/javascript
+
+
+Instructions:
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+*/
+
+function include(arr, item){
+  return arr.includes(item)
+}
