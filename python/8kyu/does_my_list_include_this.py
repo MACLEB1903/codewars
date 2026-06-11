e6fdd2ec004515dc10c93577b0116fcfe22a3e97
MARCEL <115071244+MACLEB1903@users.noreply.github.com>
2026-06-11 20:22:27 +0000

    feat: add include() solution for kata 545991b4cbae2a5fda000158

---
+++ python/8kyu/does_my_list_include_this.py
@@ -0,0 +1,16 @@
+"""
+
+Author: MARCEL EBALLAR
+GitHub: https://github.com/MACLEB1903
+
+Source:
+https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/python
+
+
+Instructions:
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+"""
+
+def include(arr, item):
+    return item in arr
